068cb765680b35ce167892c0cb124d58f04f7594
dhiraj <dhirajc1337@gmail.com>
2025-05-03 12:36:36 +0530

    no cors

--- json-api/index.js
+++ json-api/index.js
@@ -2,12 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const cors = require('cors');
-server.use(cors({
-    origin: ['https://http-api-4o6v.onrender.com', 'http://localhost:4200'], // Add your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-}));
+
 server.use(middlewares);
 server.use(router);
 server.listen(process.env.PORT || 3000, () => {
